0fa30b46cc6780bd28abb10cd5bef4a89ee25449
zouxingwu <qq@qq.com>
2020-07-15 16:07:20 +0800

    add class model

---
+++ GzxwhApi/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sinvie.M.D.U.Api.Models
+{
+    public class UserViewModel
+    {
+    }
+}
