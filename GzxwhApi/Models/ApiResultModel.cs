a4c824f855c0c0381576313ca35a36b6e620c4bc
zouxingwu <qq@qq.com>
2020-07-15 16:02:15 +0800

    edit file dir and func

---
+++ GzxwhApi/Models/ApiResultModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Sinvie.M.D.U.Api
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ApiResultModel
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public ApiResultModel()
+        {
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string status { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string log { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string data { get; set; }
+    }
+}
